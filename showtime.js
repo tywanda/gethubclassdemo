6a5034a92ba601887f155909c100edcfe1a9b90d
tywanda <tywandaharmon0328@gmail.com>
2020-01-30 16:14:11 -0500

    go

---
+++ showtime.js
@@ -0,0 +1,30 @@
+// to make the audition
+var height;
+var applicationProcess;
+height ="5";
+applicationProcess = "true";
+console.log(height + applicationProcess);
+var time;
+var place;
+time ="clock";
+place ="London";
+console.log(time + place);
+// Conditional Homework
+var count = 2;
+console.log(count + 5);
+var time = 7;
+console.log(count * time);
+// when i divide 14/3 i think i will get 2
+var place = 14;
+console.log(place % 3);
+console.log(2 % 2);
+// the answer was 0 as i expected it to be
+
+if (count %2==0){
+    console.log("flower");
+} else {
+    console.log("sun");
+}
+// this statement test the state of my variable in 4 ways
+
+    
